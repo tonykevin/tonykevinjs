7b301dca503424961efb357cd0789eb4d232d3b6
tonykevin <hi@tonykevin.com>
2019-10-01 23:14:28 -0500

    rename pug file folder

--- .config/paths.js
+++ .config/paths.js
@@ -10,7 +10,7 @@ module.exports = {
   app: resolveApp('src'),
   examples: resolveApp('examples'),
   favicon: resolveApp('src/images/favicon/tonykevinjs.png'),
-  homepage: resolveApp('src/templates/index.pug'),
+  homepage: resolveApp('src/views/pages/index.pug'),
   images: resolveApp('src/images'),
   styles: resolveApp('src/styles/main.css'),
   postcssConfig: resolveApp('.config/postcss/')
